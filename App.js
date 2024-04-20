330baba5cf77897c94284163fd24f76acfb0b4e4
morpheus-30 <konnichiwasekai7@gmail.com>
2024-04-20 12:58:14 +0530

    ndsjnk

--- App.js
+++ App.js
@@ -6,6 +6,8 @@ import {
 
 } from 'react-native';
 
+import { vh, vw, vmax, vmin } from 'react-native-viewport-units';
+
 
 
 const App = () => {
@@ -18,15 +20,16 @@ const App = () => {
     }}>
       <Image source={require('./assets/bg.png')} style={{
         width: '100%',
-        height: '70%',
+        height: '80%',
       }} />
       <Image source={require('./assets/logo.png')} style={{
         height: 233,
-        // width: 200,
-        // alignSelf: 'center',
+        width: 200,
+        alignSelf: 'center',
         // marginTop: '100',
-        // flex: 1,
-        // position: 'absolute',
+        marginTop: 100,
+        flex: 1,
+        position: 'absolute',
       }} />
 
     </View>
