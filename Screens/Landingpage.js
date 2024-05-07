c914210354b1e95dfb416578c58a4b53c8cdbc53
ath.24 <atharvadilshaan@outlook.com>
2024-05-07 23:24:11 +0530

    added the text element from line 27. (this element is yet to be placed at the right position)

--- Screens/Landingpage.js
+++ Screens/Landingpage.js
@@ -2,19 +2,20 @@ import React from 'react';
 import {
   View, Text, Image
 
-} from 'react-native';const LandingScreen= () => {
+} from 'react-native';
 
+const LandingScreen= () => {
 
-    return (
+  return (
       <View style={{
         backgroundColor: 'black',
         height: '100%',
       }}>
-        <Image source={require('./assets/bg.png')} style={{
+        <Image source={require('./../assets/bg.png')} style={{       /* yahaan (./../assets/bg.png)yeh path mere lappy mei chl gya, tere mei (./assets/bg.png) yeh lga le yaad se, varna error ayega */
           width: '100%',
           height: '80%',
         }} />
-        <Image source={require('./assets/logo.png')} style={{
+        <Image source={require('./../assets/logo.png')} style={{
           height: 233,
           width: 200,
           alignSelf: 'center',
@@ -23,7 +24,14 @@ import {
           flex: 1,
           position: 'absolute',
         }} />
-  
+        <Text style={{
+        color:'#FFFFFF', 
+        fontSize:18,
+        
+      }}
+        >
+        Effortlessly Manage Your Tasks: Add, Edit, and Track Progress Seamlessly on the Go.
+        </Text>
       </View>
   
     );
